852e0bd99d8a39b915c3276dd7df7e0f47e87450
Cham <450139220@qq.com>
2026-02-28 13:36:10 +0800

    feat: use model name to index files

--- convert.py
+++ convert.py
@@ -8,12 +8,12 @@ shape = tuple(map(int, os.environ["MODEL_INPUT"].split(",")))
 model_name = os.environ["MODEL_NAME"]
 
 # dynamic import
-spec = importlib.util.spec_from_file_location("model_module", "model.py")
+spec = importlib.util.spec_from_file_location("model_module", f"{model_name}.py")
 m = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(m)
 
 model = m.ModelStructure()
-model.load_state_dict(torch.load("best.pt", map_location="cpu"))
+model.load_state_dict(torch.load(f"{model_name}.pt", map_location="cpu"))
 model.eval()
 
 example_input = torch.randn(*shape)
